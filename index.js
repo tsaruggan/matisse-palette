368747e60d1a2ebc7a175b736ac87ed6f45d2a8f
tsaruggan <tsaruggan@gmail.com>
2022-05-21 14:07:20 -0400

    connected matisse Colour data

--- index.js
+++ index.js
@@ -1,32 +1,27 @@
+import Colour, * as matisse from "matisse";
 import { clusterize } from 'node-kmeans';
 
-var colors = [
-    [20, 20, 80],
-    [22, 22, 90],
-    [250, 255, 253],
-    [0, 30, 70],
-    [200, 0, 23],
-    [100, 54, 100],
-    [255, 13, 8]
-];
+const colours = [
+    new Colour("#67598B"),
+    new Colour("#6F4048"),
+    new Colour("#9C81B9"),
+    new Colour("#886381"),
+    new Colour("#111024"),
+    new Colour("#E7A3B9"),
+    new Colour("#4C252A"),
+    new Colour("#E48D85"),
+]
 
-// const data = [
-//     {'company': 'Microsoft' , 'size': 91259, 'revenue': 60420},
-//     {'company': 'IBM' , 'size': 400000, 'revenue': 98787},
-//     {'company': 'Skype' , 'size': 700, 'revenue': 716},
-//     {'company': 'SAP' , 'size': 48000, 'revenue': 11567},
-//     {'company': 'Yahoo!' , 'size': 14000 , 'revenue': 6426 },
-//     {'company': 'eBay' , 'size': 15000, 'revenue': 8700},
-//   ];
+let vectors = new Array();
+for (let i = 0; i < colours.length; i++) {
+    vectors[i] = [colours[i].red, colours[i].green, colours[i].blue];
+}
 
-//   // Create the data 2D-array (vectors) describing the data
-//   let vectors = new Array();
-//   for (let i = 0 ; i < data.length ; i++) {
-//     vectors[i] = [ data[i]['size'] , data[i]['revenue']];
-//   }
-
-
-clusterize(colors, { k: 4 }, (err, res) => {
+clusterize(vectors, { k: 3 }, (err, res) => {
     if (err) console.error(err);
-    else console.log('%o', res);
+    else res.forEach(element => {
+        const centroid = element.centroid;
+        const colour = Colour.RGB(centroid[0], centroid[1], centroid[2]);
+        console.log(colour.toHEX());
+    });
 });
